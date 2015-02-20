6d3676aff71e513ffe97949f51e7b112fbd077f6
unknown <gayatri.sarva@xavica.com>
2015-02-20 14:35:43 +0530

    MultiInhr Progs by G3

---
+++ Gayatri_Csharp/Gayatri_Solution/MultiInhr/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MultiInhr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            inheri obj = new inheri();
+            obj.mode();
+            obj.feature();
+            obj.Noise();
+            Console.Read();
+        }
+        class inheri : vehicle
+        {
+            public void Noise()
+            {
+                Console.WriteLine("All Vehicles Creates Noise !!--> I AM GRAND PARENT ");
+            }
+            
+        }
+        class Mode
+        {
+            public void mode()
+            {
+                Console.WriteLine("There are Many Modes of Transport !! --> I AM CHILD");
+            }
+        }
+        class vehicle : Mode
+        {
+            public void feature()
+            {
+                Console.WriteLine("They Mainly Help in Travelling !! --> I AM PARENT");
+            }
+        }
+    }
+}

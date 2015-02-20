b31101670f624aab46be08a0642cb6d16d9db8f7
unknown <gayatri.sarva@xavica.com>
2015-02-20 10:46:46 +0530

    C# Progs by G3

---
+++ DevTeam_Solution/Indexes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ClassLibrary1;
+
+namespace Indexes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            values newval = new values();
+            newval[3] = 58;
+            newval[5] = 60;
+            for (int i = 0; i < 10; i++)
+            {
+                System.Console.WriteLine("Element #{0} = {1}", i, newval[i]);
+            }
+            System.Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+}
